248e087d7b22bbf1515ea644b92b2c8f19c583f2
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-31 16:40:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECt series.title, characters.specices  = 'human'"
+  "SELECT (series.title), MAX(characters.specices) FROM SERIES WHERE species = 'human'; "
 end
 
 def select_character_names_and_number_of_books_they_are_in
