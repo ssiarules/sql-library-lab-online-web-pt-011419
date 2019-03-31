1d315fd93e351d88aa597d59451a59a060624387
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-31 18:43:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, FROM characters JOIN character_books ON characters.id = character_books.character_id, character_id = character_books.book_id DESC limit 1; "
 end
